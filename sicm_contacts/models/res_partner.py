00306edb6f69782062933a58aa03b882f59c7875
Kameron Ramaherison <kameronramaherison@MacBook-Air-de-install.local>
2025-11-20 13:40:04 +0100

    Ajout des étiquettes obligatoires

---
+++ sicm_contacts/models/res_partner.py
@@ -0,0 +1,13 @@
+
+from odoo import models, fields
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    category_id = fields.Many2many(
+        'res.partner.category',
+        column1='partner_id',
+        column2='category_id',
+        string='Tags',
+        required=True
+    )
