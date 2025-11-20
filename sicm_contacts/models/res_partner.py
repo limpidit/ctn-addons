8514033e58b5606bf2e2bf7ab91aad93e70f5cf1
Kameron Ramaherison <kameronramaherison@MacBook-Air-de-install.local>
2025-11-20 15:51:29 +0100

    Suppression paramètres inutiles

--- sicm_contacts/models/res_partner.py
+++ sicm_contacts/models/res_partner.py
@@ -5,9 +5,5 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     category_id = fields.Many2many(
-        'res.partner.category',
-        column1='partner_id',
-        column2='category_id',
-        string='Tags',
         required=True
     )
