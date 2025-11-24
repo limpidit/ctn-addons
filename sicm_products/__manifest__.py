18f3bda14f5fc1d974027e4be4db0cb22c2c9690
Kameron Ramaherison <kameronramaherison@MacBook-Air-de-install.local>
2025-11-24 16:55:12 +0100

    Vue liste par défaut pour les produits

---
+++ sicm_products/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'SICM Products',
+    'version': '1.0',
+    'description': 'SICM Products',
+    'author': 'Limpid IT',
+    'license': 'LGPL-3',
+    'depends': [
+        'sale',
+        'purchase',
+        'stock',
+        'account'
+    ],
+    'data': [
+        'views/product_views.xml'
+    ],
+    'auto_install': False,
+    'application': False
+}
