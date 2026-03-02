d24158e46ecbdd018c8c9083ebdd24de49cb6388
Alexandre Gosselin <alexandre.gosselin@limpidit.com>
2026-03-02 11:47:38 +0100

    Début import attributs

---
+++ ctn_imports/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'CTN imports',
+    'version': '1.0',
+    'description': '',
+    'summary': '',
+    'author': 'LimpidIT',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': 'tools',
+    'depends': [
+        'sale'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/import_product_attribute_wizard_views.xml',
+    ],
+    'auto_install': False,
+    'application': False,
+}
