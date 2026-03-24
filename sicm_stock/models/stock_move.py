015a3ff5c89b0b115f43f45aaed5638988c6991e
Kameron Ramaherison <kameronramaherison@MacBook-Air-de-install.local>
2026-03-24 17:22:28 +0100

    WIP

--- sicm_stock/models/stock_move.py
+++ sicm_stock/models/stock_move.py
@@ -5,11 +5,28 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     is_sicm_company = fields.Boolean(compute='_compute_is_sicm_company', store=False)
+    sale_order_notes = fields.Text(
+        string='Notes commande',
+        compute='_compute_sale_order_notes',
+        store=True,
+        readonly=False,
+    )
 
     @api.depends_context('allowed_company_ids')
     def _compute_is_sicm_company(self):
         for picking in self:
             picking.is_sicm_company = picking.env.company.is_sicm_company()
+
+    @api.depends('sale_id', 'sale_id.order_line', 'sale_id.order_line.display_type', 'sale_id.order_line.name')
+    def _compute_sale_order_notes(self):
+        for picking in self:
+            if picking.sale_id:
+                notes = picking.sale_id.order_line.sorted('sequence').filtered(
+                    lambda l: l.display_type == 'line_note' and l.name
+                )
+                picking.sale_order_notes = '\n'.join(notes.mapped('name')) if notes else False
+            else:
+                picking.sale_order_notes = False
 
 
 class StockMove(models.Model):
